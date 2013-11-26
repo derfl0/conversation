5ce350df8182dbf08cc4039c303d10cad940e8dd
Florian Bieringer <>
2013-11-26 12:29:41 +0100

    purge possibility

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,5 +1,6 @@
 <?= $output ?>
 <form>
+     <?= Studip\Button::create(_('Purge'), 'purge') ?>
     <?= Studip\Button::create(_('Simulate'), 'simulate') ?>
 <?= Studip\Button::create(_('Migration'), 'migrate') ?>
 </form>
