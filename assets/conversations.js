ea83292aabe8e85e79b8e0abdeb94c958827dc32
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-02-07 12:50:24 +0100

    version update

--- assets/conversations.js
+++ assets/conversations.js
@@ -39,7 +39,7 @@ STUDIP.conversations = {
         return STUDIP.conversations.getConversation(STUDIP.conversations.current_id);
     },
     recalcSize: function() {
-        var height = $('#main').height() - $('#head').outerHeight() - $('.message_footer').outerHeight() - $('#layout_footer').outerHeight();
+        var height = $('#main').height() - $('#head').outerHeight() - $('.message_footer').outerHeight() - $('#layout_footer').outerHeight() - 2;
         $(".scroll").height(height);
     },
     setMessageSender: function() {
