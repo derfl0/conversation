2585dd592dc466f7370bb5188e2e37a74114b020
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-06-11 07:43:17 +0200

    open conversations without closing contact box

--- assets/everywhere.js
+++ assets/everywhere.js
@@ -1,10 +1,10 @@
 STUDIP.conversations.contact = {
     init: function() {
-        $('#conversations_contact').click(function() {
+        $('#conversations_contact a').click(function() {
             if ($('#contact_box').length > 0) {
                 $('#contact_box').toggle();
             } else {
-                $(this).append('<div id="contact_box"></div>');
+                $(this).parent().append('<div id="contact_box"></div>');
                 $.ajax({
                     type: "GET",
                     url: STUDIP.conversations.getUrl("everywhere/contacts"),
@@ -17,26 +17,27 @@ STUDIP.conversations.contact = {
         });
     },
     parseJson: function(json) {
-        $.each(json, function(name, value) {
-            $('#contact_box').append($('<p>'+value+'</p>'));
+        $.each(json, function(id, value) {
+            $('#contact_box').append($('<p>' + value + '</p>').click(function() {
+                STUDIP.conversations.everywhere.open(id);
+            }));
         });
     }
 };
 
 STUDIP.conversations.everywhere = {
-    loadUrl: STUDIP.conversations.getUrl("everywhere/conversation"),
     open: function(conversation_id) {
-                        $.ajax({
-                    type: "GET",
-                    url: STUDIP.conversations.contact.loadUrl,
-                    dataType: "json"
-                }).done(function(json) {
-                    console.log(json);
-                    STUDIP.conversations.contact.parseJson(json);
-                });
-        $('#layout_footer ul').prepend($('<li class="conversation"><a>Kontakte</a></li>'));
+        /*$.ajax({
+            type: "GET",
+            url: STUDIP.conversations.getUrl("everywhere/conversation"),
+            dataType: "json"
+        }).done(function(json) {
+            console.log(json);
+            STUDIP.conversations.contact.parseJson(json);
+        });*/
+        $('#layout_footer ul').prepend($('<li class="conversation"><a>'+conversation_id+'</a></li>'));
     }
-}
+},
 
 $(document).ready(function() {
     // Captain Hook
