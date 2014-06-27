6369c5a447a6d486929790146babb638c488d5c9
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-06-27 13:19:44 +0200

    part of the big image handler

--- assets/conversations.js
+++ assets/conversations.js
@@ -307,18 +307,3 @@ STUDIP.conversations.scroll = {
         });
     }
 };
-
-STUDIP.conversations.image = {
-    apply: function() {
-        $('img.image.new').click(function(e) {
-            e.preventDefault();
-            STUDIP.conversations.image.show($(this));
-        }).removeClass('new');
-    },
-    show: function(img) {
-        $('body').append('<div class="image_overlay"><img style="max-height: ' + ($(document).height() - 20) + 'px; max-width: ' + ($(document).width() - 20) + 'px" src="' + img.attr('src') + '" /></div>');
-        $('.image_overlay').click(function() {
-            $(this).remove();
-        });
-    }
-};
