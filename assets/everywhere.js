fe3a23d1b5ce76de3869daa74887a85ae154b658
Florian Bieringer <florian.bieringer@uni-passau.de>
2014-07-01 15:02:31 +0200

    Reopen using cookie

--- assets/everywhere.js
+++ assets/everywhere.js
@@ -21,10 +21,44 @@ $(document).ready(function() {
             });
         }
     });
+    
+    STUDIP.conversations.cookie.load();
+    $.each(STUDIP.conversations.persistent, function(id, value) {
+        if (id !== null) {
+            STUDIP.conversations.open(id, value.name);
+        }
+    });
 
 });
 
+STUDIP.conversations.cookie = {
+    add: function(conversation_id, name, position) {
+        STUDIP.conversations.persistent[conversation_id] = {name: name, position: position};
+        STUDIP.conversations.cookie.store();
+    },
+    remove: function(conversation_id) {
+        delete STUDIP.conversations.persistent[conversation_id];
+        STUDIP.conversations.cookie.store();
+    },
+    load: function() {
+        STUDIP.conversations.persistent = {};
+        var value = "; " + document.cookie;
+        var parts = value.split("; conversations=");
+        if (parts.length == 2)
+            STUDIP.conversations.persistent = $.parseJSON(parts.pop().split(";").shift());
+        if (STUDIP.conversations.persistent === undefined) {
+            STUDIP.conversations.persistent = {};
+        }
+    },
+    store: function() {
+        document.cookie = "conversations=" + JSON.stringify(STUDIP.conversations.persistent) + "; path=/";
+    }
+};
+
 STUDIP.conversations.open = function(conversation_id, name) {
+
+    STUDIP.conversations.cookie.add(conversation_id, name);
+
     var conversation = $('.conversation_contact[data-contact="' + conversation_id + '"]');
     if (conversation.length > 0) {
         if (!conversation.is(":visible")) {
@@ -37,6 +71,7 @@ STUDIP.conversations.open = function(conversation_id, name) {
         // Append the close icon
         contact.append($('<img>').attr('src', STUDIP.ASSETS_URL + 'images/icons/12/blue/decline.png').click(function(event) {
             $(this).parent('li').hide();
+            STUDIP.conversations.cookie.remove(conversation_id, name, $('.conversation_contact').length);
         }));
 
         // Append the name
