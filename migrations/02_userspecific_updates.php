0ca23d9b3c9d28b64dab336cc63aa21f781ffa19
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-03-02 09:33:42 +0100

    Flush on update

--- migrations/02_userspecific_updates.php
+++ migrations/02_userspecific_updates.php
@@ -3,6 +3,8 @@ class UserspecificUpdates extends DBMigration
 {
     function up(){
         DBManager::get()->query("ALTER TABLE conversations ADD readdate int(11) NOT NULL DEFAULT '0'");
+        StudipAutoloader::addAutoloadPath(__DIR__ . '/../models');
+        Conversation::expireTableScheme();
     }
 
     function down()
